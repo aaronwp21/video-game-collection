1ce744152c3864484e4b17d66c4b27528463eead
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-03-09 20:18:51 +0000

    theme and css baseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 
+import Layout from './components/Layout';
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme/theme";
+import { CssBaseline } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<List />} />
-            <Route path='/add' element={<Add />} />
-            <Route path='/update/:id' element={<Update />} />
-            <Route path='*' element={<NotFound />} />
-          </Route>
-        </Routes>
+        <CssBaseline />
+        <ThemeProvider theme={theme}>
+          <Routes>
+            <Route path='/' element={<Layout />}>
+              <Route index element={<List />} />
+              <Route path='/add' element={<Add />} />
+              <Route path='/update/:id' element={<Update />} />
+              <Route path='*' element={<NotFound />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
       </Router>
     </div>
   )
