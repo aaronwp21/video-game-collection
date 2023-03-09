81403e0f3784051df6d79a17c3c58831d2602822
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-03-09 20:07:54 +0000

    added router layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<List />} />
+            <Route path='/add' element={<Add />} />
+            <Route path='/update/:id' element={<Update />} />
+            <Route path='*' element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   )
 }
