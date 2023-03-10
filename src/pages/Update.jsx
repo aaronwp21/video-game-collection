eb0d54f6513056ce5937a025a6cd4b9377404f5a
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-03-10 17:24:58 +0000

    added update functionality however it only works if something is changed

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { Typography } from '@mui/material'
+import GameForm from '../components/forms/GameForm'
+import { GamesContext } from '../components/contexts/game.context'
 
 function Update() {
+  const { id } = useParams();
+  const { games, updateGame } = useContext(GamesContext)
+
+  const gameFound = games.find((game) => parseInt(id) === game.id);
+
   return (
-    <div>Update</div>
+    <>
+      <Typography variant="h2" component="h1" sx={{marginBottom: 2}}>
+        Update {gameFound.title}
+      </Typography>
+      <GameForm game={gameFound} submitHandler={updateGame} />
+    </>
   )
 }
 
