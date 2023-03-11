9ab8b59edb5aed5e01b7d2709b39099eb0abf4f5
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-03-11 15:01:40 +0000

    changed API url

--- src/settings.js
+++ src/settings.js
@@ -1,2 +1,3 @@
-export const GAMES_ENDPOINT = 'http://localhost:3000/games';
+// export const GAMES_ENDPOINT = 'http://localhost:3000/games';
+export const GAMES_ENDPOINT = 'https://my-json-server.typicode.com/aaronwp21/video-game-api/games';
 export const STORAGE_KEY = 'games';
