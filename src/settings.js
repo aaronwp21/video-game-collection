16bdc77d6a0a7e6b03e963c6aa502ed028453bd5
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-03-12 12:45:05 +0000

    updated settings

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,2 @@
-// export const GAMES_ENDPOINT = 'http://localhost:3000/games';
-export const GAMES_ENDPOINT = 'https://my-json-server.typicode.com/aaronwp21/video-game-api/games';
+export const GAMES_ENDPOINT = 'http://localhost:3000/games';
 export const STORAGE_KEY = 'games';
